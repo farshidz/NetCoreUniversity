93d7111cef1177bf376d1d6cbc30bb0d4a8e9e35
Farshid Zavareh <farshid.zavareh@outlook.com>
2017-05-28 14:44:29 +1000

    Add Playground project

---
+++ Playground/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using NetCoreUniversity.Data;
+using NetCoreUniversity.Data.Models;
+
+namespace NetCoreUniversity.Playground {
+
+    public class Program {
+
+        public static void Main(string[] args)
+        {
+            var teacher = JsonConvert.DeserializeObject<Teacher>("{\"menu\": {" +
+                                                                 "\"id\": \"file\"," +
+                                                                 "\"value\": \"File\"," +
+                                                                 "\"popup\": {" +
+                                                                 "\"menuitem\": [" +
+                                                                 "{\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"}," +
+                                                                 "{\"value\": \"Open\", \"onclick\": \"OpenDoc()\"}," +
+                                                                 "{\"value\": \"Close\", \"onclick\": \"CloseDoc()\"}" +
+                                                                 "]}" +
+                                                                 "}}");
+
+            Console.ReadLine();
+        }
+
+    }
+}
